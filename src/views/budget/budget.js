863c0ea801402d47cd28d0ccd4efc341b4588278
wgeralt <wgeralt@outlook.com>
2020-06-20 23:17:14 +0800

    06.20 增加启动页

--- src/views/budget/budget.js
+++ src/views/budget/budget.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Avatar, Icon, ListItem, Header } from 'react-native-elements';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import SplashScreen from 'react-native-splash-screen'
 import deposit from './deposit'
 import withdraw from './withdraw'
 
@@ -9,6 +10,9 @@ import withdraw from './withdraw'
 const Tab = createMaterialTopTabNavigator();
 
 class budget extends Component {
+    componentDidMount() {
+        SplashScreen.hide();
+    }
     render() {
         return (
             <Tab.Navigator
